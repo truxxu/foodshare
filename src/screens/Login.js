f1fbfd352f7f2663df77cd0c6d8b7acdbf36afe6
Daniel Trujillo <danielfetrujillo@gmail.com>
2020-10-13 22:27:58 -0500

    installed and configured firebase

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -2,26 +2,26 @@ import React, {useContext} from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
 import {useForm} from 'react-hook-form';
 
-// import {Context as AuthContext} from '../context/AuthContext';
+import {Context as AuthContext} from '../context/AuthContext';
 import {InputError, Spinner} from '../atoms';
 import {LoginForm} from '../molecules';
 
 const Login = ({navigation}) => {
   const {control, handleSubmit, errors} = useForm();
-  /*  const {
+  const {
     logInUser,
     state: {isLoading, error},
   } = useContext(AuthContext);
 
   if (isLoading) {
     return <Spinner />;
-  }*/
+  }
 
   return (
     <View style={styles.container}>
       <LoginForm control={control} errors={errors} />
-      <Button title="Sign in" />
-      <InputError text={'error'} error={'error'} />
+      <Button title="Sign in" onPress={handleSubmit(logInUser)} />
+      <InputError text={error} error={error} />
       <Text
         style={styles.loginText}
         onPress={() => navigation.navigate('Register')}>
@@ -38,7 +38,6 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     padding: 35,
-    backgroundColor: '#fff',
   },
   loginText: {
     color: '#3740FE',
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
 import {useForm} from 'react-hook-form';
 
+import {Context as AuthContext} from '../context/AuthContext';
 import {InputError, Spinner} from '../atoms';
 import {RegisterForm} from '../molecules';
 
 const Register = ({navigation}) => {
   const {control, handleSubmit, errors} = useForm();
+  const {
+    registerUser,
+    state: {isLoading, error},
+  } = useContext(AuthContext);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <View style={styles.container}>
       <RegisterForm control={control} errors={errors} />
-      <Button title="Create new account" />
-      <InputError text={'error'} error={'error'} />
+      <Button title="Create new account" onPress={handleSubmit(registerUser)} />
+      <InputError text={error} error={error} />
       <Text
         style={styles.loginText}
         onPress={() => navigation.navigate('Login')}>
