c7eba65988211e1ed62514dd1d72dd90b2ece3c7
Daniel Trujillo <danielfetrujillo@gmail.com>
2020-10-13 20:28:41 -0500

    initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>FoodShare App</Text>
+    </View>
+  );
+};
+
+export default App;
